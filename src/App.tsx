3d32cdc97e319a2312383921c9942912f7c04009
karengabiviana <karengabrieli.viana@outloo.com>
2022-02-18 14:15:07 -0300

    Acrescentando About

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./pages/about/About";
+import "./pages/home/Home";
+import "./pages/login/Login";
+import "./components/statics/footer/Footer";
+import "./components/statics/navbar/Navbar";
+import "./App.css";
+import { Home } from '@material-ui/icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <Switch>
+          <div>
+          
+            
+          </div>
+        </Switch>
+      <Footer />  
+  </Router>
   );
 }
 
---
+++ src/pages/about/About.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Grid, Box, Typography,TextField } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function About()
+{
+    return(
+        <>
+            <Grid container direction="column" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" xs={6}>
+                    <Box paddingX={20}>
+                        <Box display="flex" justifyContent="center">
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" >Fonte </Typography>
+                            <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam minus similique a, laborum dolorum nisi natus delectus tenetur accusantium quod eaque illo, aperiam error numquam nulla soluta debitis. Dolor.</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" >Sobre Nós </Typography>
+                            <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam minus similique a, laborum dolorum nisi natus delectus tenetur accusantium quod eaque illo, aperiam error numquam nulla soluta debitis. Dolor.</Typography>
+                            <Box marginRight={1}>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
