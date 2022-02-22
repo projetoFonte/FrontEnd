342901d2be790854733d79a51057a321b379db4f
karengabiviana <karengabrieli.viana@outloo.com>
2022-02-22 18:51:34 -0300

    Atualizando Sobre nós

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { Grid, Box, Typography,TextField } from "@material-ui/core";
+import { Grid, Box, Typography, TextField } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-function About()
-{
-    return(
+function About() {
+    return (
         <>
-            <Grid container direction="column" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" xs={6}>
-                    <Box paddingX={20}>
-                        <Box display="flex" justifyContent="center">
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{color:"blue"}} >Fonte </Typography>
-                            <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam minus similique a, laborum dolorum nisi natus delectus tenetur accusantium quod eaque illo, aperiam error numquam nulla soluta debitis. Dolor.</Typography>
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" >Sobre Nós </Typography>
-                            <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam minus similique a, laborum dolorum nisi natus delectus tenetur accusantium quod eaque illo, aperiam error numquam nulla soluta debitis. Dolor.</Typography>
-                            <Box marginRight={1}>
-                            </Box>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid item xs={10} >
+                    
+                    <Box flexDirection="column" display="flex" justifyContent="center" alignItems="center">
+                        <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center">
+                            Projeto Fonte
+                        </Typography>
+                        <Typography variant="body1" gutterBottom color="textPrimary" component="body" align="justify" >
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam minus similique a, laborum dolorum nisi natus delectus tenetur accusantium quod eaque illo, aperiam error numquam nulla soluta debitis. Dolor.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aut eaque nobis omnis fugiat veniam autem placeat officia magnam ex consequuntur maxime, totam perferendis dignissimos ipsum animi qui consequatur accusamus.
+                            Et dolore eius quis aperiam corporis eos excepturi tempore molestias necessitatibus dolor animi beatae aliquid voluptates magnam nostrum tenetur maxime sint provident totam ab, magni officia at voluptas sit. Ipsam.
+                        </Typography>
+                        <Box flexDirection="column" display="flex" justifyContent="center" alignItems="center">
+                            <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >
+                                Sobre Nós
+                            </Typography>
+                            <Typography variant="body1" gutterBottom color="textPrimary" component="body" align="justify" >
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laboriosam minus similique a, laborum dolorum nisi natus delectus tenetur accusantium quod eaque illo, aperiam error numquam nulla soluta debitis. Dolor.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus beatae tempore, maxime officiis facilis nesciunt est id quasi hic voluptas molestiae autem quaerat, modi nihil facere omnis iste temporibus accusantium.
+                                Enim iusto suscipit itaque consectetur minima impedit temporibus. Ducimus culpa amet omnis placeat accusamus incidunt aliquam optio distinctio quos sed quisquam nam illo iusto doloremque dicta voluptas est, natus enim?
+                            </Typography>
                         </Box>
                     </Box>
                 </Grid>
