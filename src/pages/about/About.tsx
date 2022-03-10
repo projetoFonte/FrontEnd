8039af3dc63ffdd1827946f98a4ced8226705da8
Lucas Silva Braga <lucas.sbraga14@gmail.com>
2022-03-09 22:00:04 -0300

    correção da Home e escolha das fontes

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -37,25 +37,25 @@ function About() {
                 <Grid item xs={10} >
 
                     <Box flexDirection="column" display="flex" justifyContent="center" alignItems="center">
-                        <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center">
+                        <Typography className='about' variant="h4" gutterBottom color="textPrimary" component="h4" align="center">
                             Projeto Fonte
                         </Typography>
-                        <Typography variant="body1" gutterBottom color="textPrimary" component="body" align="justify" >
+                        <Typography className="texto1" variant="body1" gutterBottom color="textPrimary" component="body" align="justify" >
                             Há uma crise hídrica hoje, fomentada pelo mau gerenciamento da água,
                             pelo desmatamento da nossa "vasta" floresta amazônica e também pelo fator aquecimento global, o que ocasiona a defazagem na distribuição equitativa da água,
                             e afeta a todos.
                             Em poucos anos seremos cada vez mais atingidos com mudanças climáticas e sua consequências.
-                            Podemos reverter isso, visando os princípios universais do Pacto Global, nosso objetivo é captar organizações interessadas em impactar gerações futuras e fazer
+                            Visando os princípios universais do Pacto Global, nosso objetivo é captar organizações interessadas em impactar gerações futuras e fazer
                             a diferença, criando alertas para aumentar a visibilidade para essa questão.
                             A água é nossa fonte e ela não pode cessar.
                         </Typography>
                         <Box flexDirection="column" display="flex" justifyContent="center" alignItems="center">
-                            <Typography variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >
+                            <Typography className="sobre" variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >
                                 Sobre Nós
                             </Typography>
                             <Typography variant="body1" gutterBottom color="textPrimary" component="body" align="justify" >
 
-                               <img src="https://media-exp1.licdn.com/dms/image/D4E35AQETss4gTW6syg/profile-framedphoto-shrink_200_200/0/1637854227106?e=1646935200&v=beta&t=VL4cQuHwo5bbGtqsHVy71e7GhMw1wfNS6zyA8FCEyMI" alt="" />  <br></br>
+                                <img src="https://media-exp1.licdn.com/dms/image/D4E35AQETss4gTW6syg/profile-framedphoto-shrink_200_200/0/1637854227106?e=1646935200&v=beta&t=VL4cQuHwo5bbGtqsHVy71e7GhMw1wfNS6zyA8FCEyMI" alt="" />  <br></br>
 
                                 Karen Gabrieli (Ela ou Elu)<br></br>
                                 A Comunicadora<br></br>
