18dd37520c4cfeba6c157eac0ed506fd1f737d5d
levi <nanowebcontato@gmail.com>
2022-03-11 00:49:58 -0300

    ajustes de informações e estilizações da home

--- src/components/posts/posttab/PostTab.tsx
+++ src/components/posts/posttab/PostTab.tsx
@@ -14,32 +14,58 @@ function PostTab() {
       <TabContext value={value}>
         <AppBar position="static">
           <Tabs className='bgtab' centered indicatorColor="secondary" onChange={handleChange}>
-            <Tab label="A fonte" value="1" href="#scroll" />
-            <Tab label="Objetivo" value="1" href="#scroll1" />
-            <Tab label="Sobre nós" value="1" href="#scroll2" />
+            <Tab label="A fonte" value="1" href="#fonte" />
+            <Tab label="O projeto" value="1" href="#projeto" />
+            <Tab label="Sobre nós" value="1" href="#sobre" />
           </Tabs>
 
         </AppBar>
-        <TabPanel value="1" id="scroll">
-          <Box display="flex" flexWrap="wrap" justifyContent="center">
-            <Typography variant="body1" gutterBottom color="textPrimary" align="justify">
-              A água é nossa fonte.<br></br>
-              A água revigora.<br></br>
-              A água alimenta vidas.<br></br>
-              A água é um recurso finito.
-            </Typography>
+        <TabPanel value="1" className='bgTexto'>
+          <Box display="flex" flexDirection="row" justifyContent="space-around" alignItems="base-line">
+          <Box className='img1'></Box>
+
+            <Box display="flex" flexDirection='column' justifyContent="center">
+              <Typography id="fonte" variant="body1" gutterBottom color="textPrimary" align="justify" className='textMini textWeight'> 
+                A água é nossa <b>fonte</b>.
+              </Typography>
+              <Typography variant="body1" gutterBottom color="textPrimary" align="justify" className='textMini textWeight'>
+                A água revigora.
+              </Typography>
+              <Typography variant="body1" gutterBottom color="textPrimary" align="justify" className='textMini textWeight'>
+                A água alimenta vidas.
+              </Typography>
+              <Typography variant="body1" gutterBottom color="textPrimary" align="justify" className='textMini textWeight'>
+                A água é um recurso finito.
+              </Typography>
+            </Box>
+          </Box>
+        </TabPanel>
+
+        <TabPanel value="1" id="projeto">
+
+          <Box display="flex" justifyContent="center" alignItems="center" >
+            <Box display="flex" flexDirection='column' justifyContent="center" className='img2'>
+              <Typography className='about bold titleColor' variant="h4" gutterBottom color="textPrimary" component="h4" align="center">
+                FONTE
+              </Typography>
+              <Typography className="texto1 textSize textWeight textAlign" variant="body1" gutterBottom color="textPrimary" component="body" >
+                Há uma crise hídrica hoje, fomentada pelo mau gerenciamento da água,
+                pelo desmatamento da nossa vasta floresta amazônica e também pelo fator aquecimento global, o que ocasiona a defazagem na distribuição equitativa da água,
+                e afeta a todos.
+                Em poucos anos seremos cada vez mais atingidos com mudanças climáticas e sua consequências.
+                Visando os princípios universais do <a href="https://www.pactoglobal.org.br/" target="_blank"  rel="noreferrer" color="textPrimary" className='texto1 titulo text-decorator-none'>Pacto Global</a>, nosso objetivo é captar organizações interessadas em impactar gerações futuras e fazer
+                a diferença, criando alertas para aumentar a visibilidade para essa questão.
+                A água é nossa <b>fonte</b> e ela não pode cessar.
+              </Typography>
+            </Box>
           </Box>
 
         </TabPanel>
-        <TabPanel value="1" id="scroll1">
-
-        </TabPanel>
         
-        <TabPanel value="1" id="scroll2">
-
-
+        <TabPanel value="1" id="sobre">
           <About />
         </TabPanel>
+        
       </TabContext>
     </>
   );
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -33,40 +33,28 @@ function About() {
 
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='bgSobre'>
                 <Grid item md={10} xs={10} >
 
-                    <Box flexDirection="column" display="flex" justifyContent="center" alignItems="center">
-                        <Box>
-                            <Typography className='about' variant="h4" gutterBottom color="textPrimary" component="h4" align="center">
-                                Projeto Fonte
-                            </Typography>
-                            <Typography className="texto1" variant="body1" gutterBottom color="textPrimary" component="body" align="justify" >
-                                Há uma crise hídrica hoje, fomentada pelo mau gerenciamento da água,
-                                pelo desmatamento da nossa "vasta" floresta amazônica e também pelo fator aquecimento global, o que ocasiona a defazagem na distribuição equitativa da água,
-                                e afeta a todos.
-                                Em poucos anos seremos cada vez mais atingidos com mudanças climáticas e sua consequências.
-                                Visando os princípios universais do Pacto Global, nosso objetivo é captar organizações interessadas em impactar gerações futuras e fazer
-                                a diferença, criando alertas para aumentar a visibilidade para essa questão.
-                                A água é nossa fonte e ela não pode cessar.
-                            </Typography>
-                        </Box>
-                        <Box flexDirection="column" display="flex" justifyContent="center" >
-                            <Typography className="sobre" variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >
-                                Sobre Nós
+                       
+                        <Box flexDirection="column" display="flex" justifyContent="center" alignItems="center">
+                            <Typography className="sobre bold textColor padding" variant="h4" gutterBottom color="textPrimary" component="h4" align="center" >
+                                SOBRE NÓS
                             </Typography>
 
-                            <Box display="flex" justifyContent="space-between" alignItems="space-around">
+                            <Box display="flex" justifyContent="space-around" alignItems="center">
                                 <Grid item md={3} xs={10} >
-                                    <Box>
+                                    <Box className='padding'>
                                         <Box display="flex" justifyContent="center" >
-                                            <img src="https://media-exp1.licdn.com/dms/image/D4E35AQETss4gTW6syg/profile-framedphoto-shrink_200_200/0/1637854227106?e=1646935200&v=beta&t=VL4cQuHwo5bbGtqsHVy71e7GhMw1wfNS6zyA8FCEyMI" alt="" className="bio"/>
+                                             <img src="https://media-exp1.licdn.com/dms/image/D4E35AQETss4gTW6syg/profile-framedphoto-shrink_200_200/0/1637854227106?e=1646935200&v=beta&t=VL4cQuHwo5bbGtqsHVy71e7GhMw1wfNS6zyA8FCEyMI" alt="Karen" className="bio"/>
                                         </Box>
                                         <Typography className="tituloBio">
-                                            Karen Gabrieli
+                                            <a href="https://www.linkedin.com/in/karen-gabrieli-viana/" target="_blank" rel="noreferrer" className='text-decorator-none'>
+                                                Karen Gabrieli
+                                            </a>
                                         </Typography>
                                         <Typography className="subtituloBio" gutterBottom>
-                                            A Comunicadora
+                                            <i>A Comunicadora</i>
                                         </Typography>
                                         <Typography className="textBio">
                                             A mais velha, tem a vida organizada no <i>Notion</i>, Enérgica e dona de uma mente criativa e gráfica.
@@ -76,15 +64,17 @@ function About() {
                                 </Grid>
 
                                 <Grid item md={3} xs={10} >
-                                    <Box>
+                                    <Box className='padding'>
                                         <Box display="flex" justifyContent="center">
                                             <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF6rlcg6vclZA/profile-displayphoto-shrink_200_200/0/1607049610692?e=1652313600&v=beta&t=n_l0rOxzYqma_Ryj_WW9caowNeTSSbw8yJHhX6cpATw" alt="" className="bio"/>
                                         </Box>
                                         <Typography className="tituloBio">
+                                        <a href="https://www.linkedin.com/in/natalia-ferreira-duque/" target="_blank" rel="noreferrer" className='text-decorator-none'>
                                             Natalia F Duque
+                                        </a>
                                         </Typography>
                                         <Typography className="subtituloBio" gutterBottom>
-                                            A Detalhista
+                                            <i>A Detalhista</i>
                                         </Typography>
                                         <Typography className="textBio" >
                                             Da geração Z, Conciliadora, atenta aos detalhes e sempre querendo aprender.
@@ -93,15 +83,17 @@ function About() {
                                 </Grid>
 
                                 <Grid item md={3} xs={10} >
-                                    <Box>
+                                    <Box className='padding'>
                                         <Box display="flex" justifyContent="center">
                                             <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFSoJZAXSWlvA/profile-displayphoto-shrink_200_200/0/1646845220008?e=1652313600&v=beta&t=33I_EccaM67c1HwFT59yelTSbbJVHc78KMuStbvOHcY" alt="" className="bio" />
                                         </Box>
                                         <Typography className="tituloBio">
+                                        <a href="https://www.linkedin.com/in/gabriel-maia-21a725205/" target="_blank" rel="noreferrer" className='text-decorator-none'>
                                             Gabriel Maia
+                                        </a>
                                         </Typography>
                                         <Typography className="subtituloBio" gutterBottom>
-                                            O Observador
+                                            <i>O Observador</i>
                                         </Typography>
                                         <Typography className="textBio" >
                                             O mais novo, determinado e disposto a resolver qualquer problema, facilidade com Back-end.
@@ -109,17 +101,20 @@ function About() {
                                     </Box>
                                 </Grid>
                             </Box>
+
                             <Box display="flex" justifyContent="space-between">
                                 <Grid item md={3} xs={10} >
-                                    <Box>
+                                    <Box className='padding'> 
                                         <Box display="flex" justifyContent="center">
                                             <img src=" https://media-exp1.licdn.com/dms/image/C4E03AQFV3duPNF8f3Q/profile-displayphoto-shrink_200_200/0/1645064988888?e=1652313600&v=beta&t=DR5M5tO4sKZvonaTyzNijVldIEjJoYgtwhh0divcFLo" alt="" className="bio"/>
                                         </Box>
                                         <Typography className="tituloBio">
+                                        <a href="https://www.linkedin.com/in/williamrsh/" target="_blank" rel="noreferrer" className='text-decorator-none'>
                                             William Santos
+                                        </a>
                                         </Typography>
                                         <Typography className="subtituloBio" gutterBottom>
-                                            O Professor
+                                            <i>O Professor</i>
                                         </Typography>
                                         <Typography className="textBio" >
                                         Apaixonado por tecnologia, motivado por desafios e sempre disponível quando alguém precisa de ajuda.
@@ -128,15 +123,17 @@ function About() {
                                 </Grid>
 
                                 <Grid item md={3} xs={10} >
-                                    <Box>
+                                    <Box className='padding'>
                                         <Box display="flex" justifyContent="center">
                                             <img src="//media-exp1.licdn.com/dms/image/C4E03AQHjSV2sfqVRQw/profile-displayphoto-shrink_200_200/0/1644242914800?e=1652313600&v=beta&t=QZOwKktiJRxFoSkBOPQU_53-nE99R3TwR2QwPRYyJOY" alt="" className="bio"/>
                                         </Box>
                                         <Typography className="tituloBio" >
+                                        <a href="https://www.linkedin.com/in/levmn/" target="_blank" rel="noreferrer" className='text-decorator-none'>
                                             Levi Magni
+                                        </a>
                                         </Typography>
                                         <Typography className="subtituloBio" gutterBottom>
-                                            O Proativo
+                                            <i>O Proativo</i>
                                         </Typography>
                                         <Typography className="textBio" >
                                             Nascido um ano após o bug do milênio, naturalmente curioso, apaixonado por artes e tecnologia.
@@ -145,24 +142,25 @@ function About() {
                                 </Grid> 
 
                                 <Grid item md={3} xs={10} >
-                                    <Box>
+                                    <Box className='padding'>
                                         <Box display="flex" justifyContent="center" >
                                             <img src="//media-exp1.licdn.com/dms/image/C4E03AQE4OR1c2rmNLw/profile-displayphoto-shrink_200_200/0/1641927713221?e=1652313600&v=beta&t=lQ1RI8K024jrcdFCHUV7O3l_uKt3Bk_dTnpdnU6azSc" alt="" className="bio"/>
                                         </Box>
                                         <Typography className="tituloBio" >
+                                        <a href="https://www.linkedin.com/in/lucas-braga-25b250205/" target="_blank" rel="noreferrer" className='text-decorator-none'>
                                             Lucas Braga
+                                        </a>
                                         </Typography>
                                         <Typography className="subtituloBio" gutterBottom >
-                                            O Agitado
+                                            <i>O Agitado</i>
                                         </Typography>
                                         <Typography className="textBio" >
-                                        O amante de Peaky Blinders, sempre frio e calculista
+                                        O amante de Peaky Blinders, sempre frio e calculista.
                                         </Typography>
                                     </Box>
                                 </Grid> 
                             </Box> 
                         </Box>
-                    </Box>
                 </Grid>
             </Grid>
 
